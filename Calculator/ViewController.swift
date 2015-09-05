541f86a91c75ee0cdfe428a1d86548f7fb50b00d
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2015-09-05 11:28:16 +0700

    Renamed the IBActions to be more self-explanatory.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -40,7 +40,7 @@ class ViewController: UIViewController {
     
     // MARK: - IBAction Properties
     
-    @IBAction func appendDigitButtonPressed(sender: UIButton) {
+    @IBAction func appendDigitButton(sender: UIButton) {
         if isUserInTheMiddleOfTyping {
             if self.displayLabel.text!.characters.first == "0" {
                 if sender.currentTitle == "0" && self.displayLabel.text!.characters.contains(".") {
@@ -69,13 +69,13 @@ class ViewController: UIViewController {
             self.isUserInTheMiddleOfTyping = true
         }
     }
-    @IBAction func clearDisplayButtonPressed(sender: UIButton) {
+    @IBAction func clearDisplayButton(sender: UIButton) {
         self.floatingPointButton.enabled = true
         self.displayLabel.text = "0"
         self.isUserInTheMiddleOfTyping = false
     }
     
-    @IBAction func floatingPointButtonPressed(sender: UIButton) {
+    @IBAction func appendFloatingPointButton(sender: UIButton) {
         if self.displayLabel.text!.characters.contains(".") { return }
         else {
             self.displayLabel.text!.append("." as Character)
@@ -84,7 +84,7 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func enterButtonPressed() {
+    @IBAction func enterButton() {
         self.floatingPointButton.enabled = true
         self.isUserInTheMiddleOfTyping = false
         self.operandStack.append(self.displayValue)
@@ -92,10 +92,10 @@ class ViewController: UIViewController {
         print("self.operandStack: \(operandStack)")
     }
     
-    @IBAction func calculateButtonPressed(sender: UIButton) {
+    @IBAction func performMathOperationButton(sender: UIButton) {
         let calculationSymbol = sender.currentTitle
         
-        if self.isUserInTheMiddleOfTyping { self.enterButtonPressed() }
+        if self.isUserInTheMiddleOfTyping { self.enterButton() }
         
         switch calculationSymbol! {
             case "×": self.performMathCalculation({ (x: Double, y: Double) -> Double in return y * x })
@@ -112,14 +112,14 @@ class ViewController: UIViewController {
     func performMathCalculation(operation: (x: Double, y: Double) -> Double) {
         guard self.operandStack.count >= 2 else { return }
         self.displayValue = operation(x: self.operandStack.removeLast(), y: self.operandStack.removeLast())
-        self.enterButtonPressed()
+        self.enterButton()
     }
 
     @nonobjc // obj-c doesn't allow method overloading & this class inherits from UIViewController, which is an obj-c file despite writing it in swift.
     func performMathCalculation(operation: Double -> Double) {
         guard self.operandStack.count >= 1 else { return }
         self.displayValue = operation(self.operandStack.removeLast())
-        self.enterButtonPressed()
+        self.enterButton()
     }
 }
 
